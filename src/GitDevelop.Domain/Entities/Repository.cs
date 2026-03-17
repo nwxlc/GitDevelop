870600c4cc05ebbc7f427f69f757e3797773adce
nwxlc <jeffkiller04@mail.ru>
2026-03-17 20:26:47 +0200

    Исправил название свойства

--- src/GitDevelop.Domain/Entities/Repository.cs
+++ src/GitDevelop.Domain/Entities/Repository.cs
@@ -4,20 +4,20 @@ public sealed class Repository
 {
     public Guid Id { get; private init; }
     public string Name { get; private init; }
-    public string DefaultBrancName { get; private init; }
+    public string DefaultBranchName { get; private init; }
     public RepositoryMetadata Metadata { get; private init; }
     public DateTime CreatedAt { get; private init; }
 
     public Repository(
         Guid id,
         string name,
-        string defaultBrancName,
+        string defaultBranchName,
         RepositoryMetadata metadata,
         DateTime createdAt)
     {
         Id = id;
         Name = name;
-        DefaultBrancName = defaultBrancName;
+        DefaultBranchName = defaultBranchName;
         Metadata = metadata;
         CreatedAt = createdAt;
     }
